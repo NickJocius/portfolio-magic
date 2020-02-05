1728b54065544093a56672af6302a019061dc23f
Nick Jocius <capacity512@yahoo.com>
2020-02-05 10:12:29 -0600

    Add files via upload

---
+++ SpeedConverter/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double miles = SpeedConverter.toMilesPerHour(11.5);
+
+        SpeedConverter.printConversion(miles);
+    }
+}
