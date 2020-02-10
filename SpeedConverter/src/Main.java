4085bee5bb0fd972aa69ca7121e536df51256044
Nick Jocius <capacity512@yahoo.com>
2020-02-09 18:52:37 -0600

    Added java folder

--- SpeedConverter/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        double miles = SpeedConverter.toMilesPerHour(11.5);
-
-        SpeedConverter.printConversion(miles);
-    }
-}
