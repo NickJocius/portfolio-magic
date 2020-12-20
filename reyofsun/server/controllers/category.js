f01e30f3975800f4f6c02c74c55493c810d04aa7
Nick Jocius <capacity512@yahoo.com>
2020-12-19 19:49:22 -0600

    added admin crud to server

---
+++ reyofsun/server/controllers/category.js
@@ -0,0 +1,56 @@
+const Category = require('../models/category');
+const slugify = require('slugify');
+
+
+exports.create = async (req, res) => {
+    try {
+        // create new category with name and slug
+        const { name } = req.body;
+        const category = await new Category({ name, slug: slugify(name).toLowerCase() }).save();
+        res.json({ category });
+
+    } catch (error) {
+        res.status(400).send('Create category failed');
+    }
+};
+
+exports.list = async (req, res) => {
+
+    try {
+        // get all categories
+        res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
+    } catch (error) {
+        res.status(400).send('Categories request failed');
+    }
+};
+
+exports.read = async (req, res) => {
+    try {
+        // get a single category
+        let category = await Category.findOne({ slug: req.params.slug }).exec();
+        res.json(category);
+    } catch (error) {
+        res.status(400).send('Category not found');
+    }
+};
+
+exports.update = async (req, res) => {
+    try {
+        // update category
+        const { name } = req.body;
+        const updated = await Category.findByIdAndUpdate({ slug: req.params.slug }, { name: name, slug: slugify(name) }, { new: true });
+        res.json(updated);
+    } catch (error) {
+        res.status(400).send('Category update failed');
+    }
+};
+
+exports.remove = async (req, res) => {
+    try {
+        // remove category
+        const deleted = await Category.findByIdAndDelete({ slug: req.params.slug });
+        res.json(deleted);
+    } catch (error) {
+        res.status(400).send('Category deletion failed');
+    }
+};
